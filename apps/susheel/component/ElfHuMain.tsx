d2ead880a430fa7199c01231fa0971e2db419be1
Susheel Rai <susheelrai01@Susheels-MacBook-Pro.local>
2025-08-09 23:50:30 +0530

    dp3

--- apps/susheel/component/ElfHuMain.tsx
+++ apps/susheel/component/ElfHuMain.tsx
@@ -1,5 +1,5 @@
 'use client'
-import THREE from 'three';
+import * as THREE from 'three';
 import React, { Suspense, useEffect, useState, useRef } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
