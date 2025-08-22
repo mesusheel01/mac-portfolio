3442fa631e2706e8a7f66b4d2aafb4b69f75fb7b
Susheel Rai <susheelrai01@Susheels-MacBook-Pro.local>
2025-08-23 00:53:21 +0530

    fixed the ssr and client collision error

--- apps/susheel/app/page.tsx
+++ apps/susheel/app/page.tsx
@@ -1,3 +1,4 @@
+import Blogs from '@/component/Blogs';
 import { Home } from '@/component/Home';
 import React from 'react'
 
@@ -6,6 +7,7 @@ const page= () => {
     <div className='bg-neutral-950 flex items-center justify-center'>
       <div  className='bg-neutral-900 w-[800px] min-h-screen'>
       <Home />
+      <Blogs />
       </div>
     </div>
   )
--- apps/susheel/component/Home.tsx
+++ apps/susheel/component/Home.tsx
@@ -25,7 +25,6 @@ export const Home = () => {
       <Aboutme />
       <Work />
       <Techstack />
-      <Blogs />
     </motion.div>
   )
 }
