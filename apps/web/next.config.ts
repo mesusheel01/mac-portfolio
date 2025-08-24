482690c8e98d547a11f4a1232e23963d726f1228
Susheel Rai <susheelrai01@Susheels-MacBook-Pro.local>
2025-08-25 04:49:44 +0530

    d1.2

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    outputFileTracingIncludes: {
+      '/**/*': ['./node_modules/.prisma/**/*', './node_modules/@prisma/**/*'],
+    },
+  },
 };
 
 export default nextConfig;
