ed3653d0c9fde0aa879b085f8561ccdb4c346ecb
Susheel Rai <susheelrai01@Susheels-MacBook-Pro.local>
2026-01-19 03:54:58 +0530

    added some padding to who is app!

--- apps/whoisme/app/page.tsx
+++ apps/whoisme/app/page.tsx
@@ -6,7 +6,7 @@ import React from 'react'
 const page = () => {
   return (
     <div className='bg-black flex items-center justify-center'>
-      <div className='bg-neutral-950 w-[800px] min-h-screen'>
+      <div className='bg-neutral-950 p-4 w-[800px] min-h-screen'>
         <Home />
         <BlogSection />
         <Footer />
