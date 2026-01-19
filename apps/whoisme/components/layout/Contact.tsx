f214b94024a193c7403575f092b9b5de2fadb53d
Susheel Rai <susheelrai01@Susheels-MacBook-Pro.local>
2026-01-20 03:48:35 +0530

    contact bug fix

--- apps/whoisme/components/layout/Contact.tsx
+++ apps/whoisme/components/layout/Contact.tsx
@@ -6,7 +6,7 @@ export const ContactSection = () => {
         <div className='flex mt-10 justify-center gap-2 font-sono'>
             <Link href={"https://github.com/mesusheel01"} target='_blank' className='text-neutral-400 hover:text-neutral-600 transtion-all duration-300'>Github</Link>
             <Link href={"https://x.com/SusheelRai58945"} target='_blank' className='text-neutral-400 hover:text-neutral-600 transtion-all duration-300'>Twitter</Link>
-            <Link href={"https://x.com/SusheelRai58945"} target='_blank' className='text-neutral-400 hover:text-neutral-600 transtion-all duration-300'>LinkedIn</Link>
+            <Link href={"https://www.linkedin.com/in/susheel-rai-3b7a51227/"} target='_blank' className='text-neutral-400 hover:text-neutral-600 transtion-all duration-300'>LinkedIn</Link>
             <Link href={"https://www.youtube.com/@TechWechDev"} target='_blank' className='text-neutral-400 hover:text-neutral-600 transtion-all duration-300'>Youtube</Link>
         </div>
     )
