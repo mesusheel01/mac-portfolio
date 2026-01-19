b9bad2065aef1cdebcad744c2e32b691969c55f5
Susheel Rai <susheelrai01@Susheels-MacBook-Pro.local>
2026-01-19 06:59:03 +0530

    Implemented Reach out/contact section!

--- apps/whoisme/components/features/home/AboutMe.tsx
+++ apps/whoisme/components/features/home/AboutMe.tsx
@@ -16,17 +16,20 @@ export const Aboutme = () => {
       {/* under dev */}
 
       <div className='m-2 mt-5 flex flex-col gap-2'>
-        <div>
+        <div className='flex justify-between'>
           <h1 className='text-purple-400 font-share-mono text-xl'>
             sushe.el
-          </h1>
-          {/* contact section */}
-          <div className='flex'>
+          </h1> {/* contact section */}
+          <div className='flex gap-2 font-sono'>
+            <Link href={"https://github.com/mesusheel01"} target='_blank' className='text-neutral-700 hover:text-neutral-500 transtion-all duration-300'>Github</Link>
+            <Link href={"https://x.com/SusheelRai58945"} target='_blank' className='text-neutral-700 hover:text-neutral-500 transtion-all duration-300'>Twitter</Link>
+            <Link href={"https://x.com/SusheelRai58945"} target='_blank' className='text-neutral-700 hover:text-neutral-500 transtion-all duration-300'>LinkedIn</Link>
+            <Link href={"https://www.youtube.com/@TechWechDev"} target='_blank' className='text-neutral-700 hover:text-neutral-500 transtion-all duration-300'>Youtube</Link>
 
           </div>
         </div>
         <p className='text-neutral-400 font-sono'>
-          is a developer who is passionate about building things for real-world use. He focus on solving problems and building effective solutions. Currently, he is developing <Link href='https://daycraft-ai.vercel.app' className='font-semibold text-purple-400 hover:text-purple-600 active:text-purple-700 transition-colors'>DayCraft.ai</Link> its an AI day planner.<br />Also touching some <span className='text-purple-900 '>Machine learning</span> looking forward to contributing to the AI-driven future..  </p>
+          is a developer who is passionate about building things for real-world use. He focus on solving problems and building effective solutions. His recent tool is <Link href='https://daycraft-ai.vercel.app' className='font-semibold text-purple-500 hover:text-purple-800 active:text-purple-600 transition-colors'>DayCraft.ai</Link> its an AI day planner. Plan your day with just one prompt, hit the link and try yourself.</p>
       </div>
     </div>
   )
